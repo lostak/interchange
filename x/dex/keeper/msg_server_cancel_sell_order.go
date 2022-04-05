e686549780d022f9732acde2f98ed7ae22de73ae
Bradley Lostak <lostak@github.com>
2022-04-04 23:00:40 -0700

    add cancel sell order

--- x/dex/keeper/msg_server_cancel_sell_order.go
+++ x/dex/keeper/msg_server_cancel_sell_order.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/lostak/interchange/x/dex/types"
@@ -10,8 +11,35 @@ import (
 func (k msgServer) CancelSellOrder(goCtx context.Context, msg *types.MsgCancelSellOrder) (*types.MsgCancelSellOrderResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	//retrieve the book
+	pairIndex := types.OrderBookIndex(msg.Port, msg.Channel, msg.AmountDenom, msg.PriceDenom)
+	s, found := k.GetSellOrderBook(ctx, pairIndex)
+	if !found {
+		return &types.MsgCancelSellOrderResponse{}, errors.New("the pair doesn't exist")
+	}
+	// check order creator
+	order, err := s.Book.GetOrderFromID(msg.OrderID)
+	if err != nil {
+		return &types.MsgCancelSellOrderReponse{}, err
+	}
 
+	if order.Creator != msg.Creator {
+		return &types.MsgCancelSellOrderResponse{}, errors.New("canceller must be creator")
+	}
+
+	//Remove Order
+	if err := s.Book.RemoveOrderFromID(msg.OrderID); err != nil {
+		return &types.MsgCancelSellOrderResponse{}, err
+	}
+	k.SetSellOrderBook(ctx, s)
+
+	// Refund seller with remaining amount
+	seller, err := sdk.AccAddressFromBech32(order.Creator)
+	if err != nil {
+		return &types.MsgCancelSellOrderResponse{}, err
+	}
+	if err := k.SafeMint(ctx, msg.Port, msg.Channel, seller, msg.AmountDenom, order.Amount); err != nil {
+		return &types.MsgCancelSellOrderResponse{}, err
+	}
 	return &types.MsgCancelSellOrderResponse{}, nil
 }
